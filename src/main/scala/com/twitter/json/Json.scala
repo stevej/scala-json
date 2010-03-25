704bf5be980337cf63c6e7d71ea74dc753099508
Matt Sanford <matt@twitter.com>
2010-03-25 14:49:25 -0700

    Move the character quoting to a function for speed and profit

--- src/main/scala/com/twitter/json/Json.scala
+++ src/main/scala/com/twitter/json/Json.scala
@@ -77,12 +77,22 @@ private class JsonParser extends JavaTokenParsers {
  * recursive Seq or Map. You are in flavor country.
  */
 object Json {
+  private[json] def quotedChar(codePoint: Int) = {
+    codePoint match {
+      case c if c > 0xffff =>
+        val chars = Character.toChars(c)
+        "\\u%04x\\u%04x".format(chars(0).toInt, chars(1).toInt)
+      case c if c > 0x7e => "\\u%04x".format(c.toInt)
+      case c => c.toChar
+    }
+  }
+
   /**
    * Quote a string according to "JSON rules".
    */
   def quote(s: String) = {
     val charCount = s.codePointCount(0, s.length)
-    "\"" + 0.to(charCount-1).map{idx =>
+    "\"" + 0.to(charCount - 1).map { idx =>
       s.codePointAt(idx) match {
         case 0x0d => "\\r"
         case 0x0a => "\\n"
@@ -90,11 +100,7 @@ object Json {
         case 0x22 => "\\\""
         case 0x5c => "\\\\"
         case 0x2f => "\\/"     // to avoid sending "</"
-        case c if c > 0xffff =>
-          val chars = Character.toChars(c)
-          ("\\u%04x" format chars(0).asInstanceOf[Int]) + ("\\u%04x" format chars(1).asInstanceOf[Int])
-        case c if c > 0x7e => "\\u%04x" format c.asInstanceOf[Int]
-        case c => c.toChar
+        case c => quotedChar(c)
       }
     }.mkString("") + "\""
   }
@@ -124,7 +130,7 @@ object Json {
   /**
    * Parses a JSON String representation into its native Scala reprsentation.
    */
-  def parse(s: String): Any = (new JsonParser).parse(s)
+  def parse(s: String): Any = (new JsonParser).parse(s) 
 }
 
 
