cf7b605b105983646d9ba247438013f0aa74ad50
Jeremy Cloud <jeremy.cloud@me.com>
2010-12-24 10:13:15 -0500

    scala 2.8.1 and latest specs

--- project/build/ScalaJsonProject.scala
+++ project/build/ScalaJsonProject.scala
@@ -3,8 +3,8 @@ import com.twitter.sbt._
 
 class ScalaJsonProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs = buildScalaVersion match {
-    case "2.7.7" => "org.scala-tools.testing" %  "specs" % "1.6.2.1" % "test"
-    case _ =>       "org.scala-tools.testing" %% "specs" % "1.6.5"   % "test"
+    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+    case _ =>       "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
   }
 
   override def subversionRepository = Some("http://svn.local.twitter.com/maven-public/")
